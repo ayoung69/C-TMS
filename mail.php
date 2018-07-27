1dfd2fec2ed9332088bf891ff4aaca530776b322
ayoung69 <39391834+ayoung69@users.noreply.github.com>
2018-07-27 17:35:16 +0900

    Add files via upload

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n Phone: $phone   \n Message: $message";
 $recipient = "doriskwon@c-tms.com";
-$subject = "Contact Form";
+$subject = "appointment-form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!" . " -" . "<a href='contact.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
