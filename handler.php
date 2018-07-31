21d72bbda97492ab403ffe091e404351516cce06
ayoung69 <39391834+ayoung69@users.noreply.github.com>
2018-07-31 11:14:26 +0900

    Add files via upload

--- handler.php
+++ handler.php
@@ -21,6 +21,6 @@ $validator->field('message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('doriskwon@c-tms.com'); // ← Your email here
+$pp->sendEmailTo('doris@c-tms.com'); // ← Your email here
 
 echo $pp->process($_POST);
