d260c514a87110a4cd054d60dc22bae704776fd8
ayoung69 <39391834+ayoung69@users.noreply.github.com>
2018-07-31 15:18:34 +0900

    Add files via upload

---
+++ contact2.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+
+     <title>CTMS</title>
+<!--
+
+CTMS
+
+http://www.tooplate.com/view/2098-health
+
+-->
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="author" content="Tooplate">
+
+
+
+     <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <link rel="stylesheet" href="form.css" >
+        <script src="form.js"></script>
+  <!-- MAIN CSS -->
+     <link rel="stylesheet" href="css/tooplate-style.css">
+	  <link rel="stylesheet" href="css/tooplate-style-c.css">
+      <link rel="stylesheet" href="style.css">
+
+    
+
+</head>
+
+<body>
+
+
+
+
+
+ <!-- PRE LOADER -->
+     <section class="preloader">
+          <div class="spinner">
+
+               <span class="spinner-rotate"></span>
+              </div>
+     </section>
+
+       <!-- MENU -->
+  
+<section id= "topheader">
+    <div class="logo"> 
+     <a href="index.html"><img src="images/logo.jpg" srcset="images/logo.jpg"   width="181" height="79"> </a>
+        <div class="author"> 
+          <div class="author-info"> </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  
+
+							  <!-- MENU LINKS -->
+                                   
+                                   
+          <div class="gnav-info"> 
+            <div class="gnav"> 
+              <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="intro.html" >회사소개</a> 
+                  
+        <ul>
+          <li><a href="intro.html">인사말</a></li>
+          <li><a href="company.html">회사개요</a></li>
+          <li><a href="customer.html">주요 고객사</a></li>
+        </ul>
+                </li>
+                <li><a href="service.html" >서비스</a> 
+                  
+        <ul>
+          <li><a href="service.html#service1">SM기술용역</a></li>
+          <li><a href="service.html#service2">SM부대사업</a></li>
+        </ul>
+                </li>
+                <li><a href="rd.html">R&amp;D</a> 
+                  
+        <ul>
+          <li><a href="rd.html">CTMS&nbsp; R&amp;D</a></li>
+          <li><a href="rd.html#rd2">컨텐츠 안내</a></li>
+        </ul>
+                </li>
+                <li><a href="location.html" >커뮤니티</a> 
+                  
+        <ul>
+          <li><a href="location.html">오시는 길</a></li>
+         <li><a href="contact.html">상담문의</a></li>
+        </ul>
+                </li>
+              </ul>
+              <div class="author"> 
+                <div class="author-info"> </div>
+              </div>
+            </div>
+          </div>
+  
+     </section>
+     
+     <!-- COMPANY -->
+     
+    <section id="locationpic" class="pic" data-stellar-background-ratio="1">
+    
+       <!-- COMPANY THUMB -->
+                   <div class="l-item">
+                         <div class="caption">  
+                          <h3 class="pic-h3">contact us</h3>  
+                           <h1 class="pic-h1">COMMUNITY</h1>
+                            </div>
+                            </div>
+                            </div>
+            </section>
+               
+            
+                                <!-- side menu -->
+                                 <section id= "content">
+                                    <div class="container">
+                                           <div class="row">
+                                         <div class="lnav">
+                                         <ul id="myUL">
+                                          <li><a href="location.html">오시는 길</a></li>
+                                          <li><a href="contact.html">상담문의</a></li>
+                                        </ul> 
+                                        </div>
+                                        </div>
+                                        </div>
+                                        
+                                        
+                                        
+                                        
+       <!-- INTRO -->
+                        
+         
+              <div class="company" data-stellar-background-ratio="2">
+             <div class="container">
+               <div class="row">
+                                     
+                                  <div class="company-info">
+                                  <div class="vl"> </div>
+                              <h2 class="wow fadeInUp" data-wow-delay="0.6s">상담문의 </h2>
+                              <br>
+                               <div class="wow fadeInUp" data-wow-delay="0.8s">
+                               
+                               
+  
+
+	
+<section id="appoint" data-stellar-background-ratio="3">
+
+	
+
+
+    <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    
+                    <form class="contact-form" method="post" action="contactform.php">
+                      <input type="text" name="name" placeholder="Full name">
+                          <input type="text" name="mail" placeholder="Your e-mail">
+                          <input type="text" name="subject" placeholder="Subject">
+                           <textarea name="message" placeholder="message"></textarea>
+                           <button type="button" name="button"></button>
+                           </form>
+                           </div>
+                           </div>
+                           </div>
+                           </section>
+                           
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                     <!-- 
+                      
+                            <label for="name"> Name:</label>
+                            <input type="text" class="form-control" id="name" name="name" required maxlength="50">
+                        </div>
+                        <div class="form-group">
+                            <label for="email"> Email:</label>
+                            <input type="email" class="form-control" id="email" name="email" required maxlength="50">
+                        </div>
+                        <div class="form-group">
+                            <label for="name"> Message:</label>
+                            <textarea class="form-control" type="textarea" name="message" id="message" placeholder="Your Message Here" maxlength="6000" rows="7"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-lg btn-success pull-right" id="btnContactUs">submit</button>
+                    </form>
+                    <div id="success_message" style="width:100%; height:100%; display:none; "> <h3>Sent your message successfully!</h3> </div>
+                    <div id="error_message" style="width:100%; height:100%; display:none; "> <h3>Error</h3> Sorry there was an error sending your form. </div>
+                </div>
+            </div>
+        </div>
+                </div>
+            </div>
+        </div>
+  </section>
+  
+  
+  
+
+
+
+
+
+     <!-- FOOTER -->
+       
+  <section id="foot1"> 
+     <div class= "companyfoot"> 
+    <footer data-stellar-background-ratio="3">
+          <div class="container">
+               <div class="row">
+      
+                    <div class="col-md-12 col-sm-12 border-top ">
+                         <div class="col-md-10 col-sm-10">
+                              <div class="copyright-text"> 
+                                    <p><h4> (주)시티엠에스</h4><p> <h5>부산광역시 남구 신선로 365 용당동 부경대학교용당캠퍼스 창업보육센터 503호 &nbsp;   대표이사 김장길</h5></p>
+ <p>Copyrightⓒ CTMS Co., Ltd. All Rights Reserved. </p>
+                              </div>
+                         </div>
+                         
+                     
+
+         <div class="col-md-2 col-sm-2 text-align-center">
+                              <div class="angle-up-btn"> 
+                                  <a href="#top" class="smoothScroll wow fadeInUp" data-wow-delay="1.2s"><i class="fa fa-angle-up"></i></a>
+                              </div>
+                         </div>   
+                    </div>
+                    
+               </div>
+          </div>
+          </p>
+          </p>
+     </footer>
+</section>
+
+
+     <!-- SCRIPTS -->
+  
+     <script src="js/jquery.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+     <script src="js/jquery.sticky.js"></script>
+     <script src="js/jquery.stellar.min.js"></script>
+     <script src="js/jquery.magnific-popup.min.js"></script>
+     <script src="js/magnific-popup-options.js"></script>
+     <script src="js/wow.min.js"></script>
+     <script src="js/smoothscroll.js"></script>
+     <script src="js/owl.carousel.min.js"></script>
+     <script src="js/custom.js"></script>
+      <script src="js/form.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+       
+        <script src="form.js"></script>
+        
+  <script>
+
+
+
+
+
+</body>
+</html>
