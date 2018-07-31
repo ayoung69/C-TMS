fc5554e41928eab0e7635c96d88ea637e935cd41
ayoung69 <39391834+ayoung69@users.noreply.github.com>
2018-07-31 16:54:38 +0900

    Update handler.php

--- handler.php
+++ handler.php
@@ -21,6 +21,6 @@ $validator->field('message')->maxLength(6000);
 
 
 
-$fh->sendEmailTo(['doriskwon@c-tms.com', 'veryartyoung@naver.com',]); // ← Your email here
+$pp->sendEmailTo('doriskwon@c-tms.com'); // ← Your email here
 
 echo$fh->process($_POST);
