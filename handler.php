9afaa0152a311c0fd2c6b12aa1e8cb61df27a6ee
ayoung69 <39391834+ayoung69@users.noreply.github.com>
2020-05-16 15:27:40 +0900

    Add files via upload

---
+++ handler.php
@@ -0,0 +1,30 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+/*
+Tested working with PHP5.4 and above (including PHP 7 )
+
+ */
+require_once './vendor/autoload.php';
+
+use FormGuide\Handlx\FormHandler;
+
+
+$fh = new FormHandler(); 
+
+$validator = $fh->getValidator();
+$validator->fields(['name','email'])->areRequired()->maxLength(50);
+$validator->field('email')->isEmail();
+$validator->field('message')->maxLength(6000);
+
+
+
+
+
+
+
+$fh->sendEmailTo(['a-artcenter@naver.com']);
+
+
+echo $fh->process($_POST);
